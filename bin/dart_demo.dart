1d812d1a3c7abd67100a8b927f7e90b371f2f7a6
Faisal Kabir Galib <faisalkabirru@gmail.com>
2023-04-12 01:08:52 +0600

    data type finished.

--- bin/dart_demo.dart
+++ bin/dart_demo.dart
@@ -1,1 +1,26 @@
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  // String, bool, int, double,
+
+  // Collection Data type
+
+  // List -> Linked List,HashMap, Set
+
+  dynamic name = "mike";
+
+  name = 3;
+
+  // ul, ol
+  List<dynamic> students = ["student1", "student2", "student3", 4, 5.6];
+  List student1 = ['name', 'addre', 'phone'];
+  List<String> phones = ["phone1", "phone2", "phone3"];
+
+  // map
+
+  Map<int, String> stu1 = {0: "SAbbir ", 1: "-03494"};
+
+  print(stu1[0]);
+
+  // print("Student - ${student1["name"]} ; phone - ${phones[0]}");
+
+  // Map
+}
